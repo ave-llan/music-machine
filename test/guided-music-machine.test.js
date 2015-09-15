4a374dd61cc4f3c198d230d204f0caba17f56bc0
John Leszczynski <jrleszczynski@gmail.com>
2015-09-15 14:15:38 -0400

    add basic tests after initialization

--- test/guided-music-machine.test.js
+++ test/guided-music-machine.test.js
@@ -1,8 +1,14 @@
 var test = require('tape')
 var GuidedMusicMachine = require('../lib/guided-music-machine.js')
 var GrammarGraph = require('grammar-graph')
+var cfGrammar = require('../grammars/cantus-firmus.js')
 
 test('GuidedMusicMachine', function (t) {
+  var cfGraph = new GrammarGraph(cfGrammar)
+  var guide = new GuidedMusicMachine(cfGraph.createGuide('Start'), 'D minor', [1])
+  t.equal(guide.tonic(), 'D')
+  t.false(guide.isComplete())
+  t.deepEqual(guide.construction(), [])
 
   t.end()
 })
