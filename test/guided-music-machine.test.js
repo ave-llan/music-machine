d832fb97eed70deb0c5844a6c17bc29b7cb0d210
John Leszczynski <jrleszczynski@gmail.com>
2015-09-15 16:10:10 -0400

    for choices, map intervals to pitches

--- test/guided-music-machine.test.js
+++ test/guided-music-machine.test.js
@@ -2,13 +2,23 @@ var test = require('tape')
 var GuidedMusicMachine = require('../lib/guided-music-machine.js')
 var GrammarGraph = require('grammar-graph')
 var cfGrammar = require('../grammars/cantus-firmus.js')
+var Key = require('nmusic').Key
 
-test('GuidedMusicMachine', function (t) {
+test('GuidedMusicMachine initializaiton', function (t) {
   var cfGraph = new GrammarGraph(cfGrammar)
-  var guide = new GuidedMusicMachine(cfGraph.createGuide('Start'), 'D minor', [1])
+  var guide = new GuidedMusicMachine(cfGraph.createGuide('Start'),
+                                     new Key('D', 'minor'),
+                                     [1])
   t.equal(guide.tonic(), 'D')
   t.false(guide.isComplete())
   t.deepEqual(guide.construction(), [])
+  t.deepEqual(guide.choices(), ['D'])
+
+  var guide2 = new GuidedMusicMachine(cfGraph.createGuide('Start'),
+                                      new Key('Eb', 'major'),
+                                      [1, 3, 5])
+  t.equal(guide2.tonic(), 'Eb')
+  t.deepEqual(guide2.choices(), ['Eb', 'G', 'Bb'])
 
   t.end()
 })
