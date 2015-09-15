eb881b19cadd7b2712681bd686b28a03c17158d8
John Leszczynski <jrleszczynski@gmail.com>
2015-09-15 12:10:54 -0400

    add initial testing

---
+++ test/guided-music-machine.test.js
@@ -0,0 +1,8 @@
+var test = require('tape')
+var GuidedMusicMachine = require('../lib/guided-music-machine.js')
+var GrammarGraph = require('grammar-graph')
+
+test('GuidedMusicMachine', function (t) {
+
+  t.end()
+})
